c1bf6c1e1b009250701d57602098efd37e20dfc5
Xrypt0 <42627045+xrypt0@users.noreply.github.com>
2021-10-02 17:12:30 +0300

    Create md5.rb

---
+++ Ruby/md5.rb
@@ -0,0 +1,17 @@
+require 'digest/md5'
+
+#This code belongs the Xrypt0
+
+puts "Please Enter The Message You Want To Encrypt :"
+message = gets.chomp
+
+def encryption(msg)
+	encrypted_message = Digest::MD5.hexdigest("#{msg}")
+	puts "Encrypted Message: #{encrypted_message} \n \n"
+	message_length = "#{msg}".length
+	#puts message_length If You Activate This Line, It Directly Prints The Number Of Characters Of The Text To Be Encrypted. 
+	puts "#{message_length}-Character Text Is Encrypted With MD5 As 32 Characters. \n \nFor More : https://github.com/xrypt0"
+end
+
+
+encryption message
