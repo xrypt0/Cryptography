024e93a3d8f5dbbbaf552376efe7e2dd6f5bfcb3
Xrypt0 <42627045+xrypt0@users.noreply.github.com>
2023-10-23 10:55:33 +0300

    Delete C/hah.php
    deleted.

--- C/hah.php
+++
@@ -1,8 +0,0 @@
-<svg onload="alert(1)" viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg">
-  <circle
-    cx="100"
-    cy="100"
-    r="100"
-    onclick="alert('You have clicked the circle.')" />
-    <iframe sandbox="allow-same-origin allow-scripts allow-popups allow-forms" src="http://usercontent.example.net/getusercontent.cgi?id=12193"></iframe>
-</svg>
